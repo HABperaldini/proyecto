0ad39d3734b75458c05512ea2e37542b57aaa516
stefano.peraldini <stefano.peraldini@hackaboss.com>
2024-04-02 20:31:17 +0200

    Front y configuracion prettier y eslint

--- back/server.js
+++ back/server.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const { PORT } = process.env;
 
 // commentario no correcto
-app.get("/", (req, res) => {
-  res.status(200).send("¡Hola!");
+app.get('/', (req, res) => {
+    res.status(200).send('¡Hola!');
 });
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
