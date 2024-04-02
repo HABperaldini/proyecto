051c6a31418eb7ed85019d4ec0898a7716723323
stefano.peraldini <stefano.peraldini@hackaboss.com>
2024-04-02 21:06:10 +0200

    Endpoint login

--- back/server.js
+++ back/server.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
     res.status(200).send('¡Hola!');
 });
 
+app.get('/login', (req, res) => {
+    res.status(200).send('¡Loginnrrrrrrr!');
+});
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
