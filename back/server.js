1fc2ee77cc8765dc8d47a0e78094daaf7eb90592
stefano.peraldini <stefano.peraldini@hackaboss.com>
2024-04-02 21:21:02 +0200

    Cambio en destructuring del env

--- back/server.js
+++ back/server.js
@@ -7,7 +7,7 @@ import express from 'express';
 
 const app = express();
 
-const { PORT } = process.env;
+const { PORT, b } = process.env;
 
 // commentario no correcto
 app.get('/', (req, res) => {
