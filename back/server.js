f527ab75fa930d0658b2e7996d9d2bcc2d092539
stefano.peraldini <stefano.peraldini@hackaboss.com>
2024-04-02 19:49:57 +0200

    Servidor básico Express

--- back/server.js
+++ back/server.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("¡Hola mundo!");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+const { PORT } = process.env;
+
+app.get("/", (req, res) => {
+  res.status(200).send("¡Hola!");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
